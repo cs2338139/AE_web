f3972d6d69525d8d0dba93e783af5abad82d6c3b
cs2338139 <cs2338139@gmail.com>
2022-04-28 19:05:46 +0800

    enter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  variants: {
+    extend: {},
+  },
   content: [],
   theme: {
     extend: {},
+    screens: {
+      sm: { min: "640px", max: "767px" },
+      md: { min: "768px", max: "1023px" },
+      lg: { min: "1024px", max: "1279px" },
+      xl: { min: "1280px", max: "1535px" },
+      "2xl": { min: "1536px" },
+    },
   },
   plugins: [],
-}
+};
