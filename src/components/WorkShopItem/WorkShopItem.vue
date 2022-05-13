94187d87c96e7176a9d6c9ec5953e49e1a601f72
cs2338139 <64711409+cs2338139@users.noreply.github.com>
2022-05-13 10:33:33 +0800

    enter

--- src/components/WorkShopItem/WorkShopItem.vue
+++ src/components/WorkShopItem/WorkShopItem.vue
@@ -1,18 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 defineProps({
-    img: String,
-    href: String,
+  img: String,
+  href: String,
 });
 </script>
 
 <template>
-    <RouterLink :to="href" class="mb-10 w-80 bg-zinc-300 dev-border-view">
-        <div class="w-64 mx-auto mt-5 dev-border-view">
-            <img :src="img"/>
-            <div class="my-2 text-lg font-bold">彩虹色的漫遊日：采蓉染繳體驗</div>
-        </div>
-    </RouterLink>
+  <RouterLink :to="href" class="mb-10 w-80 bg-zinc-300 dev-border-view">
+    <div class="w-64 mx-auto mt-5 dev-border-view">
+      <img :src="img" />
+      <div class="mt-2 text-lg font-bold">漫遊城市：泥塑鏤空杯墊製作</div>
+      <div class="my-0 font-medium">2022.07.10 10:30－12:00</div>
+    </div>
+  </RouterLink>
 </template>
 
 <style>
@@ -22,7 +23,7 @@ defineProps({
 
 @layer components {
   .dev-border-view {
-    @apply border border-solid box-border ;
+    @apply border border-solid box-border;
   }
 }
 </style>
--- src/views/WorkShopView.vue
+++ src/views/WorkShopView.vue
@@ -11,12 +11,12 @@ const homeImage = new URL('../../public/Data/WorkShops/Home_ProjectCover01.jpg',
         <RoadItem>推廣活動</RoadItem>
         <RoadItem>工作坊</RoadItem>
     </Road>
-    <div class="mt-20 mb-12 mx-auto max-w-4xl text-lg font-bold dev-border-view">
+    <div class="max-w-4xl mx-auto mt-20 mb-12 text-lg font-bold dev-border-view">
         這次帶來的工作坊從10本繪本發想，分成分程是「繪本與人文」、「科學與創造」兩個主軸。繪本與人文由流寓工作室與果實玩樂生活實驗室兩個教育推廣團隊，
         以「自然接觸」、「童趣手作」為課程精神，帶領以自然界的材料或觀察，帶領孩子與大人們進行手作課程。「科學與創造」則由光試所與Pixelight以「鏡面反射」、
         「電路電學」、「虛擬實境」為設計精神，帶領孩子與大人們進行科學與AR、VR的奇妙視覺體驗。
     </div>
-    <div class=" dev-border-view flex flex-wrap justify-between">
+    <div class="flex flex-wrap justify-between dev-border-view">
         <WorkShopItem href="/" :img="homeImage"/>
         <WorkShopItem href="/" :img="homeImage"/>
         <WorkShopItem href="/" :img="homeImage"/>
