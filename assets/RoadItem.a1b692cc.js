d2fc42fec691e3e52b3cf60ed0a8d6f74fb633d4
cs2338139 <cs2338139@gmail.com>
2022-06-13 20:23:59 +0800

    Updates

---
+++ assets/RoadItem.a1b692cc.js
@@ -0,0 +1,1 @@
+import{_ as a,o,c as t,b as e,r as n,F as _}from"./index.6ee03393.js";const r={},c=e("span",{class:"mx-5 font-bold"},"\u3009",-1),l={class:"font-bold"};function d(s,f){return o(),t(_,null,[c,e("span",l,[n(s.$slots,"default")])],64)}var p=a(r,[["render",d]]);export{p as R};
