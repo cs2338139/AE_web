5dca9d06c38b145925ec4681e870a70413550600
cs2338139 <cs2338139@gmail.com>
2022-06-22 11:14:10 +0800

    element PC done

--- src/views/Exhibition-Book-View.vue
+++ src/views/Exhibition-Book-View.vue
@@ -13,6 +13,7 @@ export default {
     return {
       exhibitionsName: null,
       bookData: null,
+      element: null,
     };
   },
   methods: {
@@ -38,16 +39,22 @@ export default {
           this.exhibitionsName = "晚安屋";
           switch (bookID) {
             case "0":
+              this.element = "Rabbit";
               break;
             case "1":
+              this.element = "Spaceship";
               break;
             case "2":
+              this.element = "Horse";
               break;
             case "3":
+              this.element = "Robbers";
               break;
             case "4":
+              this.element = "Bear";
               break;
             case "5":
+              this.element = "Moon";
               break;
           }
           break;
@@ -55,14 +62,19 @@ export default {
           this.exhibitionsName = "夢境房";
           switch (bookID) {
             case "0":
+              this.element = "Ghost";
               break;
             case "1":
+              this.element = "Leaf";
               break;
             case "2":
+              this.element = "Leaf";
               break;
             case "3":
+              this.element = "Truck";
               break;
             case "4":
+              this.element = "Coral";
               break;
           }
           break;
@@ -108,23 +120,24 @@ export default {
         <RoadItemRouter :href="'/exhibitions/' + $route.params.exhibitionID">{{ exhibitionsName }}</RoadItemRouter>
         <RoadItem>{{ bookData.title }}</RoadItem>
       </Road>
-      <div class="grid grid-cols-5 mb-20 gap-x-16">
-        <div></div>
-
-        <div class="col-start-1 col-end-4 xl:col-end-6">
+      <div class="gap-x-16 grid grid-cols-5 mb-20">
+        <div class="xl:col-end-6 col-start-1 col-end-4">
           <ImageBox :path="'Exhibitions/' + $route.params.exhibitionID + '/' + $route.params.bookID" :img="bookData.imgs" :time="3000" :auto="false" />
         </div>
 
-        <div class="col-start-4 col-end-6 xl:col-start-1">
-          <div class="text-3xl sm:text-lg text-text-1-Color">
+        <div class="xl:col-start-1 col-start-4 col-end-6">
+          <div class="sm:text-lg text-text-1-Color text-3xl">
             {{ bookData.title }}
           </div>
-          <div class="text-xl sm:text-base text-text-1-Color">
+
+          <div class="image" :class="element"></div>
+
+          <div class="sm:text-base text-text-1-Color text-xl">
             {{ bookData.title2 }}
           </div>
           <div class="mt-3">
             <div v-for="j in bookData.author" class="mb-10" ref="target">
-              <div class="mb-3 text-xl sm:text-sm">
+              <div class="sm:text-sm mb-3 text-xl">
                 {{ j.author }}
               </div>
               <template v-for="i in j.info">
@@ -135,7 +148,7 @@ export default {
         </div>
       </div>
     </div>
-    <div class="absolute w-full bottom-0 -z-50">
+    <div class="-z-50 absolute bottom-0 w-full">
       <div class="bg-bg-0-image h-8"></div>
       <div class="bg-bg-2-Color h-56" ref="bg"></div>
     </div>
@@ -164,6 +177,39 @@ export default {
 @layer components {
   .dev {
     @apply border border-solid box-border  border-red-500;
+  }
+  .image {
+    @apply absolute bg-no-repeat bg-w100% -z-10 right-40 -top-10;
+  }
+  .Rabbit {
+    @apply w-40 bg-Rabbit-2-image  h-28 -top-0 right-32;
+  }
+  .Spaceship {
+    @apply w-36 bg-Spaceship-image  h-36;
+  }
+  .Horse {
+    @apply w-32 bg-Horse-image  h-32 right-20 -top-5;
+  }
+  .Robbers {
+    @apply w-44 bg-Robbers-image  h-24 top-5 right-20 rotate-12;
+  }
+  .Bear {
+    @apply w-24 bg-Bear-image  h-36 -top-5 right-10;
+  }
+  .Moon {
+    @apply w-32 bg-arrow-6-image  h-36  right-14;
+  }
+  .Ghost {
+    @apply w-36 bg-Ghost-image  h-36  -top-5 right-10;
+  }
+  .Leaf {
+    @apply w-36 bg-Leaf-1-image  h-36 right-10;
+  }
+  .Truck {
+    @apply w-44 bg-Truck-image  h-40 right-10  -top-5;
+  }
+  .Coral {
+    @apply w-52 bg-Coral-1-image  h-36 right-5 -top-0;
   }
 }
 </style>
