3a6dc33a5da036df6088aee62c4919a2bca4eb43
cs2338139 <cs2338139@gmail.com>
2022-06-28 11:36:18 +0800

    Updates

--- assets/NotFound-View.6cdb393f.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./ElementPanel.7cca6596.js";import{o,c as t,a as n,F as s,b as e}from"./index.453eff0f.js";const _=e("div",{class:"wrap"},[e("div",null,"404 - Page not found"),e("div",null,"Go back home.")],-1),i={setup(r){return(l,c)=>(o(),t(s,null,[n(a,{ref:"element"},null,512),_],64))}};export{i as default};
